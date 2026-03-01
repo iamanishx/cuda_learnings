fbb8befb87d2d1bfc647ac760e2bf45af7d03f05
Manish Biswal <manishbiswal754@gmail.com>
2026-03-02 00:45:07 +0530

    MinGw instead of msvc
    Signed-off-by: Manish Biswal <manishbiswal754@gmail.com>

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Build script for CUDA examples (MinGW compatible)
+setlocal
 
 echo Building CUDA Learning Examples...
 echo.
@@ -13,6 +13,8 @@ if "%~1"=="02" goto :build_02
 if "%~1"=="03" goto :build_03
 if "%~1"=="clean" goto :clean
 
+goto :help
+
 :build_all
 call :build_01
 call :build_02
@@ -22,21 +24,21 @@ goto :end
 :build_01
 echo Building 01_vector_add.cu...
 nvcc -ccbin %CCBIN% -o 01_vector_add.exe 01_vector_add.cu
-if %errorlevel% neq 0 (echo Build failed! & exit /b 1)
+if errorlevel 1 (echo Build failed! & goto :end)
 echo Built: 01_vector_add.exe
 goto :end
 
 :build_02
 echo Building 02_matrix_mul.cu...
 nvcc -ccbin %CCBIN% -o 02_matrix_mul.exe 02_matrix_mul.cu
-if %errorlevel% neecho Build failed!q 0 ( & exit /b 1)
+if errorlevel 1 (echo Build failed! & goto :end)
 echo Built: 02_matrix_mul.exe
 goto :end
 
 :build_03
 echo Building 03_neural_network.cu...
 nvcc -ccbin %CCBIN% -o 03_neural_network.exe 03_neural_network.cu
-if %errorlevel% neq 0 (echo Build failed! & exit /b 1)
+if errorlevel 1 (echo Build failed! & goto :end)
 echo Built: 03_neural_network.exe
 goto :end
 
@@ -47,4 +49,5 @@ goto :end
 
 :help
 echo Usage: build.bat [all, 01, 02, 03, clean]
+
 :end
